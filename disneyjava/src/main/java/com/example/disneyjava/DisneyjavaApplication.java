e46548360850a2b12cceb0a6d84bde3f004ff49f
rocio melian <rocioanahimelian@gmail.com>
2021-07-07 15:58:53 -0300

    primer commit

---
+++ disneyjava/src/main/java/com/example/disneyjava/DisneyjavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.disneyjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyjavaApplication.class, args);
+	}
+
+}
---
+++ disneyjava/src/test/java/com/example/disneyjava/DisneyjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.disneyjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisneyjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
